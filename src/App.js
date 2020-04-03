e90e41742f5270d421c82b865713416a03a97002
ZenpoDev <krittamet0404@gmail.com>
2020-04-03 10:39:26 +0700

    add Menubar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button, Container } from 'reactstrap';
 import './App.css';
+import Menubar from './component/Menubar';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Menubar />
+      <Container>
+        <h1>Krittamet</h1>
+      </Container>
     </div>
-  );
+
+  )
 }
 
 export default App;
